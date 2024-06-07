fdcc57551ecbd5e67ce710abb9e8559b6f53707c
raygdev <101662915+raygdev@users.noreply.github.com>
2024-06-07 19:28:29 -0400

    Add firebase config and export auth and db

---
+++ src/firebase.configi.js
@@ -0,0 +1,22 @@
+import {
+    getAuth,
+    getFirestore
+
+} from "firebase/firestore"
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBevnpBBWnexT2K9k3B1s4YAQG2OvEu5ZA",
+    authDomain: "poeticpreserves.firebaseapp.com",
+    projectId: "poeticpreserves",
+    storageBucket: "poeticpreserves.appspot.com",
+    messagingSenderId: "314267636156",
+    appId: "1:314267636156:web:2c5dac4824e8345fa95bb9",
+    measurementId: "G-8S9YG2XN8L"
+  };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+// const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+    
