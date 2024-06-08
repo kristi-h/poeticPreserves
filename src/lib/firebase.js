d6880a4db326c603e836fa611554a69d71206656
raygdev <101662915+raygdev@users.noreply.github.com>
2024-06-07 21:07:19 -0400

    Add createAccount and login and return promises

---
+++ src/lib/firebase.js
@@ -0,0 +1,35 @@
+import { 
+    createUserWithEmailAndPassword,  
+    signInWithEmailAndPassword 
+} from "firebase/auth"
+import { auth } from '../firebase.config'
+
+export function createAccount(user) {
+    const { email, password } = user
+    return new Promise((resolve, reject) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              resolve(userCredential)
+              console.log(`account created for ${user.email}`)
+            })
+            .catch((error) => {
+                reject(error)
+                console.error(error.message)
+            })
+    })
+}
+
+export function loginAccount(user) {
+    const { email, password } = user
+    return new Promise((resolve, reject) => {
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            resolve(userCredential)
+            console.log("successfully logged in")
+          })
+          .catch((error) => {
+              reject(error)
+              console.error(error.message)
+          })
+    })
+}
