2ca0de0d96349457c91243a00164f7633ac2a16c
raygdev <101662915+raygdev@users.noreply.github.com>
2024-06-08 23:24:24 -0400

    Remove unused state and functions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,26 +2,7 @@ import React from 'react'
 import AddEntry from '../components/AddEntry'
 import Login from '../components/Login'
 
-export default function Home({poem, user, createAcct, loginAcct, savePoem }){
-
-  const onChangeHandler = (e) => {
-    setPoem({
-        ...poem,
-        [e.target.name]: e.target.value 
-    })
-    console.log('poem', poem)
-  }
-
-  const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-          await savePoem(poem)
-          //clear the form
-          e.target.reset() 
-        } catch(e) {
-          console.log(e)
-        }
-    } 
+export default function Home(){ 
 
     return(
         <div>
