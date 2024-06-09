5cb4fbf59852d9f01b237cb1cb6e04458b693a7b
raygdev <101662915+raygdev@users.noreply.github.com>
2024-06-08 23:25:14 -0400

    Remove props from login and remove AddEntry

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,10 @@
-import React from 'react'
-import AddEntry from '../components/AddEntry'
 import Login from '../components/Login'
 
 export default function Home(){ 
 
     return(
         <div>
-            <Login 
-             user= {user}
-             createClicked={createAcct}
-             loginClicked={loginAcct}
-            />
-            <AddEntry 
-                clickSubmit = {handleSubmit}
-                onChangeHandler = {onChangeHandler}
-                poem= {poem}
-                user= {user}
-            />
+            <Login />
         </div>
         
     )
