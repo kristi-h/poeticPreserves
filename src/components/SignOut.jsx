9ea142b17346a93a939da13ac55d8657c4ba2a1a
raygdev <101662915+raygdev@users.noreply.github.com>
2024-06-08 23:10:38 -0400

    Create SignOut button

---
+++ src/components/SignOut.jsx
@@ -0,0 +1,17 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase.config";
+
+export default function SignOut() {
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth)
+        } catch(e) {
+            console.log(e)
+        }
+    }
+    return (
+        <button className="sign-out" onClick={handleSignOut}>
+            Sign Out
+        </button>
+    )
+}
