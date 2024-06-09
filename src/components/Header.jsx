8bf705f1f8207c2d2fb52679d5633b24955aefaf
raygdev <101662915+raygdev@users.noreply.github.com>
2024-06-08 23:12:49 -0400

    Create a basic header with nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { useUser } from "../hooks/useUser";
+import SignOut from "./SignOut";
+
+
+export default function Header() {
+    const { user } = useUser()
+    return (
+        <header>
+            <nav>
+                <ul>
+                    {
+                        !user &&
+                        <>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='register'>Register</Link></li>
+                        </>
+                    }
+                    {
+                        user && <SignOut />
+                    }
+                </ul>
+            </nav>
+        </header>
+    )
+}
